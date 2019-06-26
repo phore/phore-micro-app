e4d4fc88af2e865a9019912b0f9c38e1518157a2
Matthias <m@tth.es>
2019-06-26 15:47:33 +0200

    added response code to result

--- src/Response/JsonResponse.php
+++ src/Response/JsonResponse.php
@@ -13,12 +13,14 @@ class JsonResponse implements Response
 {
 
     protected $jsonData;
+    protected $httpResponseCode;
     protected $header = [
         "Content-Type" => "application/json; charset=utf-8",
     ];
 
-    public function __construct(array $jsonData, array $header=null)
+    public function __construct(array $jsonData, array $header=null, int $httpResponseCode=200)
     {
+        $this->httpResponseCode = $httpResponseCode;
         if ($header !== null) {
             $this->header = array_merge($this->header, $header);
         }
@@ -28,6 +30,7 @@ class JsonResponse implements Response
     
     public function send() : bool
     {
+        http_response_code($this->httpResponseCode);
         foreach ($this->header as $key => $value)
             header("{$key}: {$value}");
         echo phore_json_encode($this->jsonData);
